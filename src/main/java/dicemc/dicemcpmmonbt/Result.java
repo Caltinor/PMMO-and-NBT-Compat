72210c6f12c8c44f752c675d0688fe54d1626cc4
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-04-18 06:12:23 -0400

    comment removal

--- src/main/java/dicemc/dicemcpmmonbt/Result.java
+++ src/main/java/dicemc/dicemcpmmonbt/Result.java
@@ -34,30 +34,11 @@ public class Result {
 		});
 		this.comparator = comparator;
 	}
-	
-	/*public Result(JsonObject obj, String comparison) {
-		res = obj.get("key").getAsString();
-		//TODO convert predicate to an array and return multiple evaluations
-		String op = obj.get("predicate").getAsJsonObject().get("operator").getAsString();
-		for (int i = 0; i < Operator.values().length; i++) {
-			if (Operator.values()[i].toString().equalsIgnoreCase(op)) {
-				operator = Operator.values()[i];
-				break;
-			}
-		}
-		JsonObject pred = obj.get("predicate").getAsJsonObject();
-		if (!pred.get("operator").getAsString().equalsIgnoreCase("EXISTS"))
-			comparator = pred.get("comparator").getAsString();
-		JsonObject valueInputs = obj.get("value").getAsJsonObject();
-		valueInputs.entrySet().forEach((e) -> {
-			values.put(e.getKey(), e.getValue().getAsDouble());
-		});
-		this.comparison = comparison;
-	}*/
-	
+
 	public boolean compares() {
 		switch (operator) {
 		case EQUALS: {
+			System.out.println(comparator+":"+comparison);
 			return comparator.equals(comparison);
 		}
 		case GREATER_THAN: {
